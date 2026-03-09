56338b41f2562416145f59608a450c6e0c588ed1
Vineet Ramshetty <vineet.ramshetty@gmail.com>
2026-03-09 18:53:53 +0530

    stayora: poc uploaded

---
+++ stayora/app.js
@@ -0,0 +1,89 @@
+const mongoose=require("mongoose");
+const Listing=require("./models/listing.js");
+const path=require("path");
+const methodOverride=require("method-override");
+const ejsMate=require("ejs-mate");
+const express=require("express");
+const app=express();
+
+let port=3000;
+
+const MONGO_URL="mongodb://127.0.0.1:27017/stayora";
+
+main().then((res)=>{
+    console.log("connected to DB");
+}).catch((err)=>{
+    console.log(err);
+});
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+};
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "/public")));
+app.engine("ejs", ejsMate);
+
+app.get("/", (req, res)=>{
+    res.send("Hi, I am root!");
+});
+
+// app.get("/testListing", async (req, res)=>{
+//     let sampleListing=new Listing({
+//         title:"Sample Villa",
+//         description:"sample sample",
+//         price:6969,
+//         location:"Hyderabad, Telangana",
+//         country:"India"
+//     });
+//     await sampleListing.save();
+//     console.log("sample saved");
+//     res.send("sample saved");
+// });
+
+app.get("/listings", async(req, res)=>{
+    let allListings=await Listing.find({});
+    res.render("listings/index", {allListings});
+});
+
+app.post("/listings", async(req, res)=>{
+    let listing=new Listing(req.body.listing);
+    await listing.save();
+    res.redirect("/listings");
+});
+
+app.get("/listings/new", (req, res)=>{
+    res.render("listings/new");
+});
+
+app.get("/listings/:id", async(req, res)=>{
+    let {id}=req.params;
+    let listing=await Listing.findById(id);
+    res.render("listings/show", {listing});
+});
+
+app.put("/listings/:id", async(req, res)=>{
+    let {id}=req.params;
+    let listing=await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+app.delete("/listings/:id", async(req, res)=>{
+    let {id}=req.params;
+    let deletedListing=await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+});
+
+app.get("/listings/:id/edit", async(req, res)=>{
+    let {id}=req.params;
+    let listing=await Listing.findById(id);
+    res.render("listings/edit", {listing});
+});
+
+app.listen(port, ()=>{
+    console.log(`app is listening on port: ${port}`);
+});
---
+++ stayora/models/listing.js
@@ -0,0 +1,29 @@
+const mongoose=require("mongoose");
+
+const listingSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String
+    },
+    image:{
+        type:String,
+        default:"https://images.unsplash.com/vector-1748299151892-8c49a62321f2?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        set: (v)=> v===""?"https://images.unsplash.com/vector-1748299151892-8c49a62321f2?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D":v
+    },
+    price:{
+        type:Number
+    },
+    location:{
+        type:String
+    },
+    country:{
+        type:String
+    }
+});
+
+const Listing=new mongoose.model("Listing", listingSchema);
+
+module.exports=Listing;
